19df18a040d06cfd727a47f28f70f3e959e6ecfd
steveybaby <steveybaby@users.noreply.github.com>
2025-09-15 06:51:02 -0700

    Update site URL for custom domain ccc.sharpers.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://steveybaby.github.io' : undefined,
-  base: process.env.CI ? '/ccc-photos' : undefined,
+  site: process.env.CI ? 'https://ccc.sharpers.com' : undefined,
   output: 'static',
   trailingSlash: 'ignore'
 });
